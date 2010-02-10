5b36f568855265b5b61601c49c36acbf274daaec
Jamal Soueidan <jamal@soueidan.com>
2010-02-10 14:44:54 +0100

    Option to send to all or one player

--- socket/ruby/requests/GameObjectRequest.rb
+++ socket/ruby/requests/GameObjectRequest.rb
@@ -3,6 +3,10 @@ require File.expand_path(File.dirname(__FILE__)) + "/Request"
 class GameObjectRequest < Request  
   def execute
     p "Executing GameObjectRequest"
-    send_all(@object)
+    if @object["to_player_id"]
+      send_to(@object["to_player_id"], @object)
+    else
+      send_all(@object)
+    end
   end
 end
